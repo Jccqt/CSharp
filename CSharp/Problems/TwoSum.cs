3e8e31339ce85177b4c48e6cf791f5f093f613de
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-08-09 20:57:28 +0800

    Added CSharp directory
    Added TwoSum problem

---
+++ CSharp/Problems/TwoSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Problems
+{
+    public class TwoSum
+    {
+        public int[] GetTwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (dictionary.ContainsKey(complement))
+                {
+                    return new int[] { dictionary[complement], i };
+                }
+
+                dictionary[nums[i]] = i;
+            }
+
+            throw new ArgumentException("No match found!");
+        }
+    }
+}
