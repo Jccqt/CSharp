9373b4a3b9d6c473719f23a5007a1a27e80a7147
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-08-14 08:37:22 +0800

    Added ContainsDuplicate problem

---
+++ CSharp/Problems/Arrays and Hashing/ContainsDuplicate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Problems.Arrays_and_Hashing
+{
+    public class ContainsDuplicate
+    {
+        // Problem: Given an integer array nums, return true if any value appears at least twice in the array,
+        // and return false if every element is distinct
+
+        // Example 1: Input nums = [1,2,3,1]
+        // Output: true
+        // Explanation: The element 1 occurs at the indices 0 and 3
+
+        // Example 2: Input nums = [1,2, 3, 4]
+        // Output false
+        // Explanation: All elements are distinct
+        public bool CheckContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (!set.Add(nums[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
