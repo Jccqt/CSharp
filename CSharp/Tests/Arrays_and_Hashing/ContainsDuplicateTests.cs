528768d71474d66fd4a40148f38ae208069880e3
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-09-24 08:57:12 +0800

    Added unit tests for Contains Duplicate problem

---
+++ CSharp/Tests/Arrays_and_Hashing/ContainsDuplicateTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CSharp.Problems.Arrays_and_Hashing;
+
+namespace CSharp.Tests.Arrays_and_Hashing
+{
+    public class ContainsDuplicateTests
+    {
+        [Fact]
+        public void SingleCase()
+        {
+            var solver = new ContainsDuplicate();
+            var result = solver.CheckContainsDuplicate(new int[] { 1, 2, 3, 1 });
+
+            Assert.True(result);
+        }
+    }
+}
