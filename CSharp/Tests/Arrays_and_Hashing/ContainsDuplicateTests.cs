f6090cd32d913f52add76baad1f97dcdf1e3474d
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-09-25 08:33:48 +0800

    Update unit tests for Contains Duplicate problem

--- CSharp/Tests/Arrays_and_Hashing/ContainsDuplicateTests.cs
+++ CSharp/Tests/Arrays_and_Hashing/ContainsDuplicateTests.cs
@@ -11,12 +11,21 @@ namespace CSharp.Tests.Arrays_and_Hashing
     public class ContainsDuplicateTests
     {
         [Fact]
-        public void SingleCase()
+        public void HasDuplicate()
         {
             var solver = new ContainsDuplicate();
             var result = solver.CheckContainsDuplicate(new int[] { 1, 2, 3, 1 });
 
             Assert.True(result);
+        }
+
+        [Fact]
+        public void HasNoDuplicate()
+        {
+            var solver = new ContainsDuplicate();
+            var result = solver.CheckContainsDuplicate(new int[] { 1, 2, 3, 4, 5 });
+
+            Assert.False(result);
         }
     }
 }
