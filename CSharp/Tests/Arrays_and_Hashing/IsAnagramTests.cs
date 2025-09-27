5a36784c023053fc2cf4234b191d7b179de44b3c
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-09-27 10:49:46 +0800

    Added unit tests for Is Anagram problem

---
+++ CSharp/Tests/Arrays_and_Hashing/IsAnagramTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp.Problems.Arrays_and_Hashing;
+using Xunit;
+
+namespace CSharp.Tests.Arrays_and_Hashing
+{
+    public class IsAnagramTests
+    {
+        [Fact]
+        public void ValidAnagram()
+        {
+            var solver = new IsAnagram();
+            var result = solver.CheckIsAnagram("anagram", "nagaram");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void InvalidAnagram()
+        {
+            var solver = new IsAnagram();
+            var result = solver.CheckIsAnagram("rat", "car");
+
+            Assert.False(result);
+        }
+    }
+}
