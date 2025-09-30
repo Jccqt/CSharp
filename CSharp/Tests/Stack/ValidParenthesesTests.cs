7b172e7a0132d78c537a4dc6232d6dae189fb5ab
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-09-30 15:04:12 +0800

    Added unit tests for Valid Parentheses problem

---
+++ CSharp/Tests/Stack/ValidParenthesesTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp.Problems.Stack;
+using Xunit;
+
+namespace CSharp.Tests.Stack
+{
+    public class ValidParenthesesTests
+    {
+        [Fact]
+        public void ValidParentheses()
+        {
+            var solver = new ValidParentheses();
+            var result = solver.IsValidParentheses("([])");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void InvalidParentheses()
+        {
+            var solver = new ValidParentheses();
+            var result = solver.IsValidParentheses("([)]");
+
+            Assert.False(result);
+        }
+    }
+}
