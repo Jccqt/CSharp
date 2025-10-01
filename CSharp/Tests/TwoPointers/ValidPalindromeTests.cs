c6e5336ed375947277291f5524c341ef3a92e5d2
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-10-01 16:36:15 +0800

    Added unit tests for Valid Palindrome problem

---
+++ CSharp/Tests/TwoPointers/ValidPalindromeTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp.Problems.TwoPointers;
+using Xunit;
+
+namespace CSharp.Tests.TwoPointers
+{
+    public class ValidPalindromeTests
+    {
+        [Fact]
+        public void ValidPalindrome()
+        {
+            var solver = new ValidPalindrome();
+            var result = solver.IsPalindrome("A man, a plan, a canal: Panama");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void InvalidPalindrome()
+        {
+            var solver = new ValidPalindrome();
+            var result = solver.IsPalindrome("race a car");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void NoTextPalindrome()
+        {
+            var solver = new ValidPalindrome();
+            var result = solver.IsPalindrome(" ");
+
+            Assert.True(result);
+        }
+    }
+}
