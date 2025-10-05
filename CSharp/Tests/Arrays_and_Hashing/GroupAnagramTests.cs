fe69653cfe6b06a4c693ca1a1d65a36c9c50a122
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-10-05 20:09:07 +0800

    Added unit tests for Group Anagram problem

---
+++ CSharp/Tests/Arrays_and_Hashing/GroupAnagramTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp.Problems.Arrays_and_Hashing;
+using Xunit;
+
+namespace CSharp.Tests.Arrays_and_Hashing
+{
+    public class GroupAnagramTests
+    {
+        [Fact]
+        public void GroupAnagram()
+        {
+            var solver = new GroupAnagrams();
+            var result = solver.GetGroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+
+            Assert.Equal(3, result.Count);
+        }
+    }
+}
