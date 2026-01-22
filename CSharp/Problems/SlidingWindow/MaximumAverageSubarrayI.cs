c8e47761a54f7f88ec532faf305a87b4ffabab82
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-01-22 17:32:25 +0800

    Added Maximum Average Subarray I problem

---
+++ CSharp/Problems/SlidingWindow/MaximumAverageSubarrayI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Problems.SlidingWindow
+{
+    internal class MaximumAverageSubarrayI
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            double currentSum = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                currentSum += nums[i];
+            }
+
+            double maxSum = currentSum;
+
+            for (int i = k; i < nums.Length; i++)
+            {
+                currentSum = (nums[i] - nums[i - k]) + currentSum;
+
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+
+            return maxSum / k;
+        }
+    }
+}
